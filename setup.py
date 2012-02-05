b32c3dfe104c340cb739e088211d0143ce44cab7
Thomas Cokelaer <cokelaer@gmail.com>
2012-02-05 15:38:35 +0000

    update setup

--- setup.py
+++ setup.py
@@ -12,17 +12,16 @@ except:
     version = 0.9
 
 data_files = [ (pj('share', 'data'), glob.glob(os.path.join('share','data', '*.dat')))]
-print data_files
 
 setup(
     name=name,
     version=version,
-    description="spectrum analysis tools",
+    description="Spectrum Analysis Tools",
+    long_description="""This package provides functions/classes to estimate Power Spectral Densities using methods based on Fourier transform, Parametric methods or eigenvalues analysis. The Fourier methods are based upon correlogram, periodogram and Welch estimates. Standard tapering windows (Hann, Hamming, Blackman) and more exotic ones are available (DPSS, Taylor, ...). The parametric methods are based on Yule-Walker, BURG, MA and ARMA, covariance and modified covariance methods. Non-parametric methods based on eigen analysis (e.g., MUSIC) and minimum variance analysis are also implemented""",
     author="Thomas Cokelaer",
     author_email="cokelaer@gmail.com",
-    url='',
+    url='http://www.assembla.com/spaces/PySpectrum/wiki',
     license='LGPL',
-
 
     packages = find_packages('src'),
     package_dir={ '' : 'src' },
@@ -30,6 +29,20 @@ setup(
     # Dependencies
     install_requires = ['matplotlib', 'numpy', 'scipy'],
     data_files = data_files,
-
+    platforms=["Linux"],
+    classifiers=["Development Status :: 1 - Planning",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: Financial and Insurance Industry",
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Telecommunications Industry",
+        "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: Unix",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Scientific/Engineering",
+        ]   
 )
 
