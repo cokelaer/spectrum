d7d0f0b8c52fa4ad39206c58433c80bd53afcd3e
Thomas Cokelaer <cokelaer@gmail.com>
2012-03-03 18:56:53 +0000

    move test_dpss to test_mtm and add test for pmtm

---
+++ test/test_mtm.py
@@ -0,0 +1,10 @@
+from spectrum.mtm import dpss, pmtm
+from spectrum import data_cosine
+
+def test_dpss():
+    dpss(64, 2.5, 4)
+
+def test_pmtm():
+    data = data_cosine(N=64, A=0.1, sampling=1024, freq=200)
+    res = pmtm(data, 2.5, 4, show=False)
+    res = pmtm(data, 2.5, show=False)
