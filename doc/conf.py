b953d651605674db075e49e218ab4d0c2419369f
cokelaer <cokelaer@gmail.com>
2018-12-27 15:53:27 +0100

    add sphinx ext

--- doc/conf.py
+++ doc/conf.py
@@ -14,7 +14,7 @@
 import sys, os
 import sphinx
 
-#sys.path.insert(0, os.path.abspath('sphinxext'))
+sys.path.insert(0, os.path.abspath('sphinxext'))
 
 import sphinx_gallery
 # If extensions (or modules to document with autodoc) are in another directory,
