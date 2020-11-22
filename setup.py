5776b68e8bd7883e8e2dc8fede2df246c77c3a82
Thomas Cokelaer <cokelaer@gmail.com>
2020-11-22 20:37:21 +0100

    remove python 3.6 from travis (issue with numpy)

--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ setup(
         "Operating System :: Unix",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         "Topic :: Scientific/Engineering",
         ]   
 )
